3a73ef998c0e60e30fa7d361bcbcc7295b9c9a52
Taylor Powell <taylorcpowell@gmail.com>
2023-01-28 13:21:07 -0500

    update linux build

--- ootfest-build-linux.sh
+++ ootfest-build-linux.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 if [ ! -f ZELOOTD.z64 ]; then
     echo "Please place rom file ZELOOTD.z64 in this directory and try again"
 	exit 1
 fi
+
+set -x
+
+cp ZELOOTD.z64 OTRExporter/ZELOOTD.z64
+
